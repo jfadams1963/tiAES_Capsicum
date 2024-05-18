2760dc7ab1f96ffedd318a54ef0e08b9c0cb09c7
Jonathan Adams <jfadams1963@proton.me>
2024-05-17 20:50:30 -0400

    Added updated encr.c

--- src/encr.c
+++ src/encr.c
@@ -117,127 +117,127 @@ encr()
 void
 cbcenc(int dirfd, char* infn, char* outfn)
 {
-    int i,r,c,s,b,bsz,ifd,ofd;
-    long sz;
-    uchar ch,pd;
-    FILE *in, *out;
+		int i,r,c,s,b,bsz,ifd,ofd;
+		long sz;
+		uchar ch,pd;
+		FILE *in, *out;
 
-    /* Get infile fd for reading */
-    ifd = openat(dirfd, infn, O_RDONLY);
+		/* Get infile fd for reading */
+		ifd = openat(dirfd, infn, O_RDONLY);
 
-    /* Open infile for reading */
-    in = fdopen(ifd, "r");
-    if ((ifd < 0) | (in == NULL) ) {
-        perror("Could not open input file for reading.\n");
-        printf("Cleaning up and exiting gracefully.\n");
-        printf("\n");
-        /* Zero out key schedule */
-        memset(w, 0, 60*4*sizeof(w[0][0]));
-        exit(EXIT_FAILURE);
-    }
+		/* Open infile for reading */
+		in = fdopen(ifd, "r");
+		if ((ifd < 0) | (in == NULL) ) {
+				perror("Could not open input file for reading.\n");
+				printf("Cleaning up and exiting gracefully.\n");
+				printf("\n");
+				/* Zero out key schedule */
+				memset(w, 0, 60*4*sizeof(w[0][0]));
+				exit(EXIT_FAILURE);
+		}
     
-    /* Size of input file */
-    (void) fseek(in, 0, SEEK_END);
-    sz = ftell(in);
-    (void) fseek(in, 0, SEEK_SET);
+		/* Size of input file */
+		(void) fseek(in, 0, SEEK_END);
+		sz = ftell(in);
+		(void) fseek(in, 0, SEEK_SET);
 
-    /* Get padding size, add to sz for byte array size. */
-    if ((sz%16) > 0) {
-        pd = (uchar) (16-(sz%16));
-    } else {
-        pd = 16;
-    }
-    bsz = (int) (sz + pd);
+		/* Get padding size, add to sz for byte array size. */
+		if ((sz%16) > 0) {
+				pd = (uchar) (16-(sz%16));
+		} else {
+				pd = 16;
+		}
+		bsz = (int) (sz + pd);
 
-    /* Next, read the bytes into a uchar array,
-     * pad with padding bytes, close input file
-	 */
-    uchar* barr = malloc(bsz);
-    for (b=0; b<sz; b++) {
-        if ((ch=fgetc(in)) != EOF) {
-            barr[b] = (uchar) ch;
-        }
-    }
-    for (b=sz; b<bsz; b++) {
-        barr[b] = (uchar) pd;
-    }
-    fclose(in);
-
-    /* Get Initialization Vector */
-    get_iv();
-
-    /* Open the outfile and write
-     * the IV to the first 16 bytes of out. 
-	 */
-    ofd = openat(dirfd, outfn, O_CREAT | O_RDWR);
-    out = fdopen(ofd, "wb");
-    if ((!ofd) | (out == NULL)) {
-        perror("out file not open for writing in cbcenc() 1!\n");
-        printf("Cleaning up and exiting gracefully.\n");
-        /* Zero out byte array */
-        memset(barr, 0, bsz*sizeof(barr[0]));
-        exit(EXIT_FAILURE); 
-    }
-    for (r=0; r<4; r++) {
-        for (c=0; c<4; c++) {
-            ch = iv[r][c];
-            fputc(ch, out);
-        }
-    }
-
-    /* Do encryption reading from byte array and write
-     * to the output file. Close file.
-	 */
-    i = 0;
-    while (i < bsz) {
-        /* Read bytes into state by _column_ ! */
-        for (c=0; c<4; c++) {
-            for (r=0; r<4; r++) {
-                st[r][c] = barr[i];
-                i++;
-            }
-        }
-
-        /* State = state xor IV */
-        for (r=0; r<4; r++) {
-            for (c=0; c<4; c++) {
-                st[r][c] = st[r][c] ^ iv[r][c];
-            }
-        }
-
-        /* Call encr() */
-        encr();
-
-        /* Copy state to next IV */
-        cpyst_iv();
-
-        /* Write bytes to outfile by _column_ !
-         * Check that the file stream is still open:
+		/* Next, read the bytes into a uchar array,
+		 * pad with padding bytes, close input file
 		 */
-        if (out == NULL) {
-            perror("out file not open for writing in cbcenc() 2!\n");
-            printf("Cleaning up and exiting gracefully.\n");
-            /* Zero out keymaterial, state and byte array */
-            memset(w, 0, 60*4*sizeof(w[0][0]));
-            memset(iv, 0, 16*sizeof(iv[0][0]));
-            memset(ns, 0, 16*sizeof(ns[0][0]));
-            memset(st, 0, 16*sizeof(st[0][0]));
-            memset(barr, 0, bsz*sizeof(barr[0]));
-            exit(EXIT_FAILURE);
-        }
-        for (c=0; c<4; c++) {
-            for (r=0; r<4; r++) {
-                ch = st[r][c];
-                fputc(ch, out);
-            }
-        }
-    }
-    fclose(out);
+		uchar* barr = malloc(bsz);
+		for (b=0; b<sz; b++) {
+				if ((ch=fgetc(in)) != EOF) {
+						barr[b] = (uchar) ch;
+				}
+		}
+		for (b=sz; b<bsz; b++) {
+				barr[b] = (uchar) pd;
+		}
+		fclose(in);
 
-    /* Zero out keymaterial, state and byte array */
-    memset(w, 0, 60*4*sizeof(w[0][0]));
-    memset(iv, 0, 16*sizeof(iv[0][0]));
-    memset(ns, 0, 16*sizeof(ns[0][0]));
-    memset(st, 0, 16*sizeof(st[0][0]));
-    memset(barr, 0, bsz*sizeof(barr[0]));
+		/* Get Initialization Vector */
+		get_iv();
+
+		/* Open the outfile and write
+		 * the IV to the first 16 bytes of out. 
+		 */
+		ofd = openat(dirfd, outfn, O_CREAT | O_RDWR);
+		out = fdopen(ofd, "wb");
+		if ((!ofd) | (out == NULL)) {
+				perror("out file not open for writing in cbcenc() 1!\n");
+				printf("Cleaning up and exiting gracefully.\n");
+				/* Zero out byte array */
+				memset(barr, 0, bsz*sizeof(barr[0]));
+				exit(EXIT_FAILURE); 
+		}
+		for (r=0; r<4; r++) {
+				for (c=0; c<4; c++) {
+						ch = iv[r][c];
+						fputc(ch, out);
+				}
+		}
+
+		/* Do encryption reading from byte array and write
+		 * to the output file. Close file.
+		 */
+		i = 0;
+		while (i < bsz) {
+				/* Read bytes into state by _column_ ! */
+				for (c=0; c<4; c++) {
+						for (r=0; r<4; r++) {
+								st[r][c] = barr[i];
+								i++;
+						}
+				}
+
+				/* State = state xor IV */
+				for (r=0; r<4; r++) {
+						for (c=0; c<4; c++) {
+								st[r][c] = st[r][c] ^ iv[r][c];
+						}
+				}
+
+				/* Call encr() */
+				encr();
+
+				/* Copy state to next IV */
+				cpyst_iv();
+
+				/* Write bytes to outfile by _column_ !
+				 * Check that the file stream is still open:
+				 */
+				if (out == NULL) {
+						perror("out file not open for writing in cbcenc() 2!\n");
+						printf("Cleaning up and exiting gracefully.\n");
+						/* Zero out keymaterial, state and byte array */
+						memset(w, 0, 60*4*sizeof(w[0][0]));
+						memset(iv, 0, 16*sizeof(iv[0][0]));
+						memset(ns, 0, 16*sizeof(ns[0][0]));
+						memset(st, 0, 16*sizeof(st[0][0]));
+						memset(barr, 0, bsz*sizeof(barr[0]));
+						exit(EXIT_FAILURE);
+				}
+				for (c=0; c<4; c++) {
+						for (r=0; r<4; r++) {
+								ch = st[r][c];
+								fputc(ch, out);
+						}
+				}
+		}
+		fclose(out);
+
+		/* Zero out keymaterial, state and byte array */
+		memset(w, 0, 60*4*sizeof(w[0][0]));
+		memset(iv, 0, 16*sizeof(iv[0][0]));
+		memset(ns, 0, 16*sizeof(ns[0][0]));
+		memset(st, 0, 16*sizeof(st[0][0]));
+		memset(barr, 0, bsz*sizeof(barr[0]));
 }
